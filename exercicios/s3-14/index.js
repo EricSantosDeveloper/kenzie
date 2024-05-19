49b4f6cd91b86c28943616a73d0a106442cef0d1
Eric Santos <99853675+EricSantosDeveloper@users.noreply.github.com>
2024-05-19 18:01:57 -0300

    1.0

---
+++ exercicios/s3-14/index.js
@@ -0,0 +1,15 @@
+// Exercício 1
+let valor1 = 10;
+let valor2 = "10";
+let comparacaoDeValores1 = valor1 === valor2;
+console.log(comparacaoDeValores1);
+
+// Exercício 2
+usuario1Altura = 1.8;
+usuario2Altura = 1.75;
+let comparacaoDeAlturas = usuario1Altura > usuario2Altura;
+console.log(comparacaoDeAlturas);
+
+// Exercício 3
+let comparacaoDeValores2 = valor1 !== valor2;
+console.log(comparacaoDeValores2);
