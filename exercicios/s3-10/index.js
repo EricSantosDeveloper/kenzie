49c95874370f96aa282273bb6bb30d894daf3c2d
Eric Santos <99853675+EricSantosDeveloper@users.noreply.github.com>
2024-05-19 17:14:26 -0300

    1.0

---
+++ exercicios/s3-10/index.js
@@ -0,0 +1,18 @@
+// Exercício 1
+let meuNome = "Eric";
+let comprimentoMeuNome = meuNome.length;
+console.log(
+  `O meu nome ${meuNome} possui ${comprimentoMeuNome} caracteres de comprimento.`
+);
+
+// Exercício 2
+let preco = 15;
+let precoFormatado = `R$${preco},00`;
+console.log(`O valor ${preco} formatado ficaria assim: ${precoFormatado}.`);
+
+// Exercício 3
+let cumprimento = "Bom dia";
+let nome = "Eric";
+let sobrenome = "Santos";
+let mensaggemDeCumprimento = `${cumprimento}, ${nome} ${sobrenome}!`;
+console.log(mensaggemDeCumprimento);
---
+++ exercicios/s3-11/index.js
@@ -0,0 +1,8 @@
+let nome = "Eric";
+let sobrenome = "Santos";
+let anoNascimento = 2001;
+let idade = 2024 - anoNascimento;
+let cidade = "Ipojuca";
+console.log(
+  `Me chamo ${nome} ${sobrenome}, tenho ou irei fazer ${idade} anos e moro em ${cidade}.`
+);
