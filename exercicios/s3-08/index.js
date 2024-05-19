0cbc2645f991f1a0a9a1196be9696cc717ae4747
Eric Santos <99853675+EricSantosDeveloper@users.noreply.github.com>
2024-05-19 15:08:49 -0300

    1.0

---
+++ exercicios/s3-08/index.js
@@ -0,0 +1,76 @@
+// Exercício 1
+let nome = "Eric";
+console.log(`Meu nome é ${nome}.`);
+
+// Exercício 2
+let sobreNome = "Santos";
+console.log(`Eu me chamo ${nome} ${sobreNome}.`);
+
+// Exercício 3
+let primeiroNome = "Eric";
+let segundoNome = "Douglas";
+let idade = 22;
+console.log(
+  `Olá, meu primeiro nome é ${primeiroNome}, meu segundo nome é ${segundoNome}, e eu tenho ${idade} anos.`
+);
+
+// Exercício 4
+let preco1 = 10;
+let preco2 = 20;
+let precoTotal = preco1 + preco2;
+console.log(
+  `Comprei uma xícara por ${preco1} reais, e dois copos por ${preco2} reais. No total, gastei ${precoTotal} reais.`
+);
+
+// Exercício 5
+let numero1 = 10;
+let numero2 = 12;
+let numeroFinal = numero1 + numero2;
+console.log(
+  `A soma do valor ${numero1} com o valor ${numero2} é igual a ${numeroFinal}.`
+);
+
+// Exercício 6
+let contador = 0;
+console.log(contador);
+contador = contador + 1;
+console.log(contador);
+contador = contador + 1;
+console.log(contador);
+contador = contador + 1;
+console.log(contador);
+contador = contador + 1;
+console.log(contador);
+contador = contador + 1;
+console.log(contador);
+
+// Exercício 7
+let anoAtual = 2024;
+let anoNascimento = 2001;
+let idadeAtual = anoAtual - anoNascimento;
+console.log(
+  `No ano de ${anoAtual}, eu tenho ou vou ter ${idadeAtual} anos de idade.`
+);
+
+// Exercício 8
+let alturaRetangulo = 5;
+let larguraRetangulo = 7;
+let areaRetangulo = larguraRetangulo * alturaRetangulo;
+console.log(
+  `Um retângulo com largura de ${larguraRetangulo}cm e altura de ${alturaRetangulo}cm possui ${areaRetangulo}cm² de área total.`
+);
+
+// Exercício 9
+let diaAtual = 19;
+let mesAtual = 5;
+let dataAtualFormatada = `${diaAtual}/${mesAtual}/${anoAtual}`;
+console.log(`A data de hoje é ${dataAtualFormatada}.`);
+
+// Exercício 10
+let valorA = 10;
+let valorB = 20;
+let valorAInvertido = valorB;
+let valorBInvertido = valorA;
+console.log(
+  `Antes a variável "valorA" possuía o valor ${valorA}, agora possui o valor ${valorAInvertido}. Antes a variável "valorB" possuía o valor ${valorB}, agora possui o valor ${valorBInvertido}.`
+);
