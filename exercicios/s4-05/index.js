55621a2eb5f86988f6980e892ca9e7ced92c62dc
Eric Santos <99853675+EricSantosDeveloper@users.noreply.github.com>
2024-05-27 23:18:12 -0300

    1.1

--- exercicios/s4-05/index.js
+++ exercicios/s4-05/index.js
@@ -6,14 +6,14 @@
 
 // console.log(somarDoisValores(15, 876));
 
-// Exercício 2
-function valorETipo(x) {
-  let tipoDoValor = typeof x;
-  return tipoDoValor;
-}
+// // Exercício 2
+// function valorETipo(x) {
+//   let tipoDoValor = typeof x;
+//   return tipoDoValor;
+// }
 
-let x = parseInt(prompt("Digite o número de sua escolha:"));
-console.log(`O valor ${x} é do tipo ${valorETipo(x)}.`);
+// let x = false;
+// console.log(`O valor ${x} é do tipo ${valorETipo(x)}.`);
 
 // Exercício 3
 function somaDosBimestres(nota1, nota2, nota3, nota4) {
@@ -27,8 +27,15 @@ let nota2 = parseInt(prompt("Digite a segunda nota:"));
 let nota3 = parseInt(prompt("Digite a terceira nota:"));
 let nota4 = parseInt(prompt("Digite a quarta nota:"));
 
-if ((nota1, nota2, nota3, nota4 == typeof Number)) {
-  console.log(somaDosBimestres(nota1, nota2, nota3, nota4));
+if (
+  nota1 === typeof Number &&
+  nota2 === typeof Number &&
+  nota3 === typeof Number &&
+  nota4 === typeof Number
+) {
+  alert(`O Resultado é ${somaDosBimestres(nota1, nota2, nota3, nota4)}`);
 } else {
-  console.log("O campo só pode receber números.");
+  alert("O campo só pode receber números.");
 }
+
+// Falta terminar
