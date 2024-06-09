0ba78268e189b375cae5ad2cc384de19abfe80d4
Eric Santos <99853675+EricSantosDeveloper@users.noreply.github.com>
2024-06-08 23:05:37 -0300

    1.0

---
+++ s4-06/index.js
@@ -0,0 +1,21 @@
+// // Exercício 1
+
+// function soma(nota1, nota2, nota3, nota4) {
+//   let soma = nota1 + nota2 + nota3 + nota4;
+//   return soma;
+// }
+
+// function media(soma) {
+//   let media = soma / 4;
+//   return media;
+// }
+
+// alert("Bem vindo a calculadora do bimestre!");
+// let nota1 = parseInt(prompt("Digite a primeira nota:"));
+// let nota2 = parseInt(prompt("Digite a segunda nota:"));
+// let nota3 = parseInt(prompt("Digite a terceira nota:"));
+// let nota4 = parseInt(prompt("Digite a quarta nota:"));
+// alert(`A média final ficou em ${media(soma(nota1, nota2, nota3, nota4))}`);
+
+// Exercício 2
+
